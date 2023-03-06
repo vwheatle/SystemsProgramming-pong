8282fa1b95d714edeb5aef41187fb4400cccea52
vwheatle <vwheatle@kent.edu>
2023-03-06 15:20:06 -0500

    try to make things more game engine shaped

--- bounce.h
+++ bounce.h
@@ -22,11 +22,88 @@ typedef struct {
 	int x, y;
 } vec2i;
 
-/** the ping pong ball **/
-
-struct ppball {
+struct ball_obj {
 	vec2i pos, dir;
-	vec2i ttm;
-	vec2i ttg;
+	vec2i old_pos;
+
+	vec2i ticks_total;
+	vec2i ticks_left;
+
 	char symbol;
+	bool redraw;
 };
+
+void ball_setup(struct ball_obj *);
+void ball_update(struct ball_obj *);
+void ball_draw(struct ball_obj *);
+
+bool bounce_or_lose(struct ball_obj *);
+
+void ball_setup(struct ball_obj *ball) {
+	ball->pos.x = X_INIT;
+	ball->pos.y = Y_INIT;
+
+	ball->old_pos = ball->pos;
+
+	ball->dir.x = 1;
+	ball->dir.y = 1;
+
+	ball->ticks_left.x = ball->ticks_total.x = X_TTM;
+	ball->ticks_left.y = ball->ticks_total.y = Y_TTM;
+
+	ball->symbol = DFL_SYMBOL;
+}
+
+void ball_update(struct ball_obj *ball) {
+	// old position
+	ball->old_pos = ball->pos;
+
+	if (ball->ticks_total.y > 0 && ball->ticks_left.y-- == 1) {
+		ball->pos.y += ball->dir.y;               // move y
+		ball->ticks_left.y = ball->ticks_total.y; // reset timer
+		ball->redraw = true;
+	}
+
+	if (ball->ticks_total.x > 0 && ball->ticks_left.x-- == 1) {
+		ball->pos.x += ball->dir.x;               // move x
+		ball->ticks_left.x = ball->ticks_total.x; // reset timer
+		ball->redraw = true;
+	}
+}
+
+void ball_draw(struct ball_obj *ball) {
+	if (ball->redraw) {
+		// erase old character
+		mvaddch(ball->old_pos.y, ball->old_pos.x, BLANK);
+		// draw new character
+		mvaddch(ball->pos.y, ball->pos.x, ball->symbol);
+
+		bounce_or_lose(ball);
+
+		move(LINES - 1, COLS - 1);
+		refresh();
+
+		ball->redraw = false;
+	}
+}
+
+bool bounce_or_lose(struct ball_obj *ball) {
+	bool r = false;
+
+	if (ball->pos.y == TOP_ROW) {
+		ball->dir.y = 1;
+		r = true;
+	} else if (ball->pos.y == BOT_ROW) {
+		ball->dir.y = -1;
+		r = true;
+	}
+	if (ball->pos.x == LEFT_EDGE) {
+		ball->dir.x = 1;
+		r = true;
+	} else if (ball->pos.x == RIGHT_EDGE) {
+		ball->dir.x = -1;
+		r = true;
+	}
+
+	return r;
+}
--- bounce2d.c
+++ bounce2d.c
@@ -6,7 +6,7 @@
  *		 	f speed up x component,  F: speed y component
  *			Q quit
  *
- *	blocks on read, but timer tick sends SIGALRM caught by ball_move
+ *	blocks on read, but timer tick sends SIGALRM caught by ball_update
  */
 
 #include <curses.h>
@@ -16,17 +16,15 @@
 
 struct {
 	WINDOW *window;
-	struct ppball ball;
+	struct ball_obj ball;
 } game;
 
 /**  the main loop  **/
 
 void set_up();
-void update();
+// void tick();
+void update(int signum);
 void wrap_up();
-
-void ball_move(struct ppball *);
-bool bounce_or_lose(struct ppball *);
 
 extern int set_ticker(int);
 
@@ -36,13 +34,13 @@ int main() {
 	int c;
 	while ((c = getchar()) != 'Q') {
 		if (c == 'f')
-			game.ball.ttm.x--;
+			game.ball.ticks_total.x--;
 		else if (c == 's')
-			game.ball.ttm.x++;
+			game.ball.ticks_total.x++;
 		else if (c == 'F')
-			game.ball.ttm.y--;
+			game.ball.ticks_total.y--;
 		else if (c == 'S')
-			game.ball.ttm.y++;
+			game.ball.ticks_total.y++;
 	}
 
 	wrap_up();
@@ -52,14 +50,7 @@ int main() {
  *	init structure and other stuff
  */
 void set_up() {
-	game.ball.pos.y = Y_INIT;
-	game.ball.pos.x = X_INIT;
-	game.ball.ttg.y = game.ball.ttm.y = Y_TTM;
-	game.ball.ttg.x = game.ball.ttm.x = X_TTM;
-	game.ball.dir.y = 1;
-	game.ball.dir.x = 1;
-	game.ball.symbol = DFL_SYMBOL;
-
+	ball_setup(&game.ball);
 	game.window = initscr();
 	noecho();
 	crmode();
@@ -76,7 +67,9 @@ void update(int signum) {
 	// disarm alarm
 	signal(SIGALRM, SIG_IGN); // don't get caught now
 
-	ball_move(&game.ball);
+	ball_update(&game.ball);
+
+	ball_draw(&game.ball);
 
 	// arm alarm
 	signal(SIGALRM, update);
@@ -85,59 +78,4 @@ void update(int signum) {
 void wrap_up() {
 	set_ticker(0);
 	endwin(); /* put back to normal	*/
-}
-
-void ball_move(struct ppball *ball) {
-	// old position
-	vec2i old;
-	old.y = ball->pos.y;
-	old.x = ball->pos.x;
-
-	// if it moved
-	bool moved = false;
-
-	if (ball->ttm.y > 0 && ball->ttg.y-- == 1) {
-		ball->pos.y += ball->dir.y; // move y
-		ball->ttg.y = ball->ttm.y;  // reset timer
-		moved = true;
-	}
-
-	if (ball->ttm.x > 0 && ball->ttg.x-- == 1) {
-		ball->pos.x += ball->dir.x; // move x
-		ball->ttg.x = ball->ttm.x;  // reset timer
-		moved = true;
-	}
-
-	if (moved) {
-		// erase old character
-		mvaddch(old.y, old.x, BLANK);
-		// draw new character
-		mvaddch(ball->pos.y, ball->pos.x, ball->symbol);
-
-		bounce_or_lose(ball);
-
-		move(LINES - 1, COLS - 1);
-		refresh();
-	}
-}
-
-bool bounce_or_lose(struct ppball *ball) {
-	bool r = false;
-
-	if (game.ball.pos.y == TOP_ROW) {
-		game.ball.dir.y = 1;
-		r = true;
-	} else if (game.ball.pos.y == BOT_ROW) {
-		game.ball.dir.y = -1;
-		r = true;
-	}
-	if (game.ball.pos.x == LEFT_EDGE) {
-		game.ball.dir.x = 1;
-		r = true;
-	} else if (game.ball.pos.x == RIGHT_EDGE) {
-		game.ball.dir.x = -1;
-		r = true;
-	}
-
-	return r;
 }
